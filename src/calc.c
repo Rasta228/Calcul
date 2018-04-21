1c188142d666bc5bac4c2e4b01a9c07d999e4806
rus <rus@russ-MacBook-Pro.local>
2018-04-21 21:26:16 +0700

    Add function Factorial

--- src/calc.c
+++ src/calc.c
@@ -2,33 +2,40 @@
 #include <stdlib.h>
 #include <math.h>
 #include "calc.h"
- 
+
 float MainFunctions (int Op, float Num1, float Num2)
 {
-	float Answer;
-	switch (Op)
-	{
-		case 1:
-		{
-			Answer=Num1+Num2;
-			break;
-		}
-		case 2:
-		{
-			Answer=Num1-Num2;
-			break;
-		}	
-		case 3:
-		{
-			Answer=Num1*Num2;
-			break;
-		}
-		case 4:
-		{
-			Answer=Num1/Num2;
-			break;
-		}
-	}
-	return Answer;
+    float Answer;
+    switch (Op)
+    {
+        case 1:
+        {
+            Answer=Num1+Num2;
+            break;
+        }
+        case 2:
+        {
+            Answer=Num1-Num2;
+            break;
+        }
+        case 3:
+        {
+            Answer=Num1*Num2;
+            break;
+        }
+        case 4:
+        {
+            Answer=Num1/Num2;
+            break;
+        }
+    }
+    return Answer;
 }
 
+int Fact(int n)
+{
+    if (n == 1)
+        return 1;
+    else
+        return n*Fact(n-1);
+}
--- src/calc.h
+++ src/calc.h
@@ -1,4 +1,5 @@
 #ifndef _calcul_calc_h
 #define _calcul_calc_h
 float MainFunctions (int Op, float Num1, float Num2);
+int Fact(int n);
 #endif
